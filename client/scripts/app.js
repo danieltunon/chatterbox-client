5d14a614557e1c7c9fe6795d090c5aacf7139741
danieltunon <danieltunon@gmail.com>
2016-03-14 20:38:11 -0700

    Pass all tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,15 @@
 // YOUR CODE HERE:
 var app = {
   server: 'https://api.parse.com/1/classes/messages',
-  init: function() {},
+  init: function() {
+    this.fetch();
+    $(document).ready(function() {
+      $('#send').on('submit', function(e) {
+        // e.preventDefault();
+        app.handleSubmit();
+      });
+    });
+  },
   send: function (data) {
     $.ajax({
       url: 'https://api.parse.com/1/classes/messages',
@@ -15,17 +23,30 @@ var app = {
     $.ajax({
       url: 'https://api.parse.com/1/classes/messages',
       type: 'GET',
-      dataType: 'json'
+      dataType: 'json',
+      complete: function(obj) {
+        obj.done(function(data) {
+          app.clearMessages();
+          _.each(data.results, function(chat) {
+            app.addMessage(chat);
+            app.addRoom(chat.roomname);
+          });
+          $('.username').on('click', function() {
+            app.addFriend();
+          }); 
+        });
+      }
     });
   },
   clearMessages: function() {
     $('#chats').empty();
   },
   addMessage: function(message) {
-    var $chat = $('<div class="poop">');
-    var $usr = $('<span>').text(message.username);
+    var $chat = $('<div class="poop chat">');
     var $msg = $('<p>').text(message.text);
-    $chat.append($usr, $msg);
+    var $usr = $('<span class="username">').text(message.username + ': ');
+    $msg.prepend($usr);
+    $chat.append($msg);
     $('#chats').append($chat);
   },
   addRoom: function(roomname) {
@@ -34,51 +55,37 @@ var app = {
     $('#roomSelect').append($room);
   },
   addFriend: function() {
-
-  }
-};   
-var load = function () {
-  $.ajax({
-    url: 'https://api.parse.com/1/classes/messages',
-    type: 'GET',
-    dataType: 'json'
-  }).done(function(data) {
-    $('#chats').html('');
-    _.each(data.results, function(chat) {
-      var $chat = $('<div class="poop">');
-      var $msg = $('<p>').text(chat.text);
-      $chat.append($msg);
-      $('#chats').append($chat);
-    }); 
-  });
-};
-
-var someData = JSON.stringify({
-  text: 'hmm',
-  roomname: 'this one'
-});
-
-var submitData = function(data) {
-  $.ajax({
-    url: 'https://api.parse.com/1/classes/messages',
-    method: 'POST',
-    contentType: 'application/json',
-    dataType: 'json',
-    data: data
-  });
-};
-
-$(document).ready(function() {
-  $('#submit-btn').on('click', function() {
+    // var $friend = 
+    console.log('poop');
+  },
+  handleSubmit: function() {
     var message = $('#message').val();
     var dataObj = {};
     dataObj.text = message;
-    submitData(JSON.stringify(dataObj));
-    return false;
-  });
-});
+    app.send(JSON.stringify(dataObj));
+    // return false;
+  }
+};   
+// var load = function () {
+//   app.fetch().done(function(data) {
+//     $('#chats').html('');
+//     _.each(data.results, function(chat) {
+//       app.addMessage(chat);
+//     }); 
+//   });
+// };
+
+// var submitData = function(data) {
+//   $.ajax({
+//     url: 'https://api.parse.com/1/classes/messages',
+//     method: 'POST',
+//     contentType: 'application/json',
+//     dataType: 'json',
+//     data: data
+//   });
+// };
 
 
-load();
+app.init();
 
-setInterval(load, 5000);
+// setInterval(load, 5000);
