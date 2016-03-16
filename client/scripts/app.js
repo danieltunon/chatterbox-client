790ac34580d944c157ce0d88c57729d4f6ef3646
danieltunon <danieltunon@gmail.com>
2016-03-15 19:08:27 -0700

    Implement filter room

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,8 +8,16 @@ var app = {
     $(document).ready(function() {
       app.fetch();
       $('#send').on('submit', function(e) {
-        // e.preventDefault();
         app.handleSubmit();
+        e.preventDefault();
+      });
+      $('#roomSelect').on('change', function(e) {
+        var selected = $(this).find('option:selected');
+        if ( selected.attr('id') === 'allmessages' ) {
+          app.fetch();
+        } else {
+          app.fetch(selected.text());
+        }
       });
     });
   },
@@ -22,18 +30,29 @@ var app = {
       data: JSON.stringify(data)
     });
   },
-  fetch: function () {
+  fetch: function (filter) {
     $.ajax({
       url: 'https://api.parse.com/1/classes/messages',
       type: 'GET',
       dataType: 'json',
+      // data: {'where':JSON.stringify({"roomname":"yellow"})},
+      data: {'where': JSON.stringify({ 'roomname': filter })},
       complete: function(obj) {
         obj.done(function(data) {
           app.clearMessages();
+          app.clearRooms();
           _.each(data.results, function(chat) {
+            // if ( filter === undefined ) {
             app.addMessage(chat);
+            // } else if ( filter === chat.roomname ) {
+              // app.addMessage(chat);
+            // };
             if ( chat.roomname && !app.rooms[chat.roomname] ) {
-              app.addRoom(chat.roomname);
+              if ( chat.roomname === filter ) {
+                app.addRoom(chat.roomname, 'selected');
+              } else {
+                app.addRoom(chat.roomname);
+              }
               app.rooms[chat.roomname] = true;
             }
           });
@@ -47,7 +66,13 @@ var app = {
   clearMessages: function() {
     $('#chats').empty();
   },
+  clearRooms: function() {
+    app.rooms = {};
+    $('#roomSelect').empty();
+    $('#roomSelect').append($('<option id="allmessages">All messages</option>'));
+  },
   addMessage: function(message) {
+    // var $tab = $('<div class=')
     var $chat = $('<div class="poop chat">');
     var $msg = $('<p>').text(message.text);
     var $usr = $('<span class="username">').text(message.username + ': ');
@@ -55,31 +80,26 @@ var app = {
     $chat.append($msg);
     $('#chats').append($chat);
   },
-  addRoom: function(roomname) {
-    var $room = $('<option>').attr('value', _.escape(roomname));
-    $room.text(roomname);
+  addRoom: function(roomname, selected) {
+    var $room = $('<option>').text(roomname);
+    if ( selected ) {
+      $room.attr('selected', 'selected');
+    }
     $('#roomSelect').append($room);
   },
   addFriend: function() {
     // var $friend =
-    console.log('poop');
   },
   handleSubmit: function() {
     var message = $('#message').val();
     var dataObj = {};
     dataObj.text = message;
+    dataObj.username = 'some string';
+    dataObj.roomname = 'nsfw';
     app.send(dataObj);
-    return false;
+    app.fetch();
   }
 };
-// var load = function () {
-//   app.fetch().done(function(data) {
-//     $('#chats').html('');
-//     _.each(data.results, function(chat) {
-//       app.addMessage(chat);
-//     });
-//   });
-// };
 
 var submitData = function(data) {
   $.ajax({
@@ -93,5 +113,14 @@ var submitData = function(data) {
 
 
 app.init();
+
+// var load = function () {
+//   app.fetch().done(function(data) {
+//     $('#chats').html('');
+//     _.each(data.results, function(chat) {
+//       app.addMessage(chat);
+//     });
+//   });
+// };
 
 // setInterval(load, 5000);
